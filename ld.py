6fb00eecb8a0bbdc1a203c31ec18fc9acbdd47f4
Dmitry Chaykin <mr.112345678900@gmail.com>
2022-05-23 23:45:31 +0300

    Первая версия

---
+++ ld.py
@@ -0,0 +1,11 @@
+phones = ["iPhone XS", "Samsung 10a", "Xiaomi Mi8"]
+
+print(phones)
+
+phones_cont = len(phones)
+print(phones_cont)
+
+phones.append("iPhone 6s")
+print(phones)
+
+print(phones.count("iPhone XS"))
